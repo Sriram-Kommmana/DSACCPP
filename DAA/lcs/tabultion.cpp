86e3a76ca5fafd92426894a05b11d0aac693827f
sriramkommana <150678282+Sriram-Kommmana@users.noreply.github.com>
2025-11-13 22:11:11 +0530

    Add LCS implementation in tabulation.cpp
    Implement Longest Common Subsequence algorithm using dynamic programming.

---
+++ DAA/lcs/tabultion.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1 = "abcde";
+    string s2 = "ace";
+
+    int n = s1.size();
+    int m = s2.size();
+
+    int dp[n+1][m+1];
+
+    // initialize to 0
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= m; j++)
+            dp[i][j] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            if (s1[i-1] == s2[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    cout << dp[n][m];
+}
